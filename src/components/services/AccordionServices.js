9f5cd4ab2507cc7d08e564150cc2c743fffc5b6d
Kike <kike@Kikes-MacBook-Pro.local>
2020-05-27 00:02:52 +0200

    Hardcode Accordion

--- src/components/services/AccordionServices.js
+++ src/components/services/AccordionServices.js
@@ -1,6 +1,6 @@
 //IMPORTS
 //-Modules
-import React from 'react'
+import React, {useState} from 'react'
 import styled from 'styled-components'
 //-Components
 import AccorionSection from './AccordionSection'
@@ -27,25 +27,82 @@ const accordionData = [
 
 
 //STYLES
+//-Variables
+const lightColour = "white"
+const smallFont = "0.75em"
+const mediumFont = "1.5625em"
+const h17o5px = "2.62368816vh"
+const w285px = "76vw"
+//-Components
 const AccordionContainer= styled.div`
     margin-top: 50vh;
     transform: translateY(-50%);
 `
+const SectionContainer= styled.div`
+    text-align: center;
+`
+const Title= styled.h2`
+    display: inline-block;
+    color: ${lightColour};
+    text-align: center;
+    padding: ${h17o5px} 0;
+    font-family: 'Gotham-Black';
+    font-size: ${mediumFont};
+    letter-spacing: 0;
+    cursor: pointer;
+`
+const Text= styled.p`
+    color: ${lightColour};
+    font-size: ${smallFont};
+    line-height: 1.3;
+    width: ${w285px};
+    text-align: center;
+    margin: 0 auto 15px;
+    display: ${props => props.active ? "inline-blockck" : "none"};;
+`
+
 
 
 function AccordionServices() {
-
-  
-
+  const [activeAccordionOne, setActiveAccordionOne] = useState(false)
+  const [activeAccordionTwo, setActiveAccordionTwo] = useState(false)
+  const [activeAccordionThree, setActiveAccordionThree] = useState(false)
+  const activeAccordionOneHandler = () => {
+    setActiveAccordionOne(!activeAccordionOne)
+    setActiveAccordionTwo(false)
+    setActiveAccordionThree(false)
+  }
+  const activeAccordionTwoHandler = () => {
+    setActiveAccordionOne(false)
+    setActiveAccordionTwo(!activeAccordionTwo)
+    setActiveAccordionThree(false)
+  }
+  const activeAccordionThreeHandler = () => {
+    setActiveAccordionOne(false)
+    setActiveAccordionTwo(false)
+    setActiveAccordionThree(!activeAccordionThree)
+  }
   return (
       <AccordionContainer>
-        {accordionData.map( section => {
+        {/* {accordionData.map( section => {
           return  <AccorionSection 
                       key={section.id} 
                       title={section.title} 
                       text={section.text} 
                   />
-        })}
+        })} */}
+        <SectionContainer onClick={() => activeAccordionOneHandler()}>
+          <Title>{accordionData[0].title}</Title>
+          <Text active={activeAccordionOne}>{accordionData[0].text}</Text>
+        </SectionContainer>
+        <SectionContainer onClick={() => activeAccordionTwoHandler()}>
+          <Title>{accordionData[1].title}</Title>
+          <Text active={activeAccordionTwo}>{accordionData[1].text}</Text>
+        </SectionContainer>
+        <SectionContainer onClick={() => activeAccordionThreeHandler()}>
+          <Title>{accordionData[2].title}</Title>
+          <Text active={activeAccordionThree}>{accordionData[2].text}</Text>
+        </SectionContainer>
       </AccordionContainer>
   )
 }
