02b80c8fac073b959f240937ddb7f0243147425c
Kike <kike@Kikes-MacBook-Pro.local>
2020-05-29 19:58:49 +0200

    Create Cross Component

---
+++ src/components/contact/Cross.js
@@ -0,0 +1,42 @@
+//IMPORTS
+//-Modules
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+//-Images
+import cross from '../../imgs/cross.svg'
+
+
+//STYLES
+//-Variables
+//-Components
+const ButtonBox = styled.div`
+    position: absolute;
+    top: 11px;
+    right: 11x;
+    width: 60px;
+    height: 60px;
+    cursor: pointer;
+`
+const CrossIcon = styled.img`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`
+
+
+//MAIN COMPONENT
+function ArrowDownButton() {
+  return (
+    <>
+      {/* Previous Path? */}
+      <Link to="/">
+        <ButtonBox>
+          <CrossIcon src={cross}/>
+        </ButtonBox>
+      </Link>
+    </>
+  )
+}
+export default ArrowDownButton
