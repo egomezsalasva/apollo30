aeb4d412b7a1856a843b028ad28d3469e723230f
Kike <kike@Kikes-MacBook-Pro.local>
2020-05-29 23:46:20 +0200

    Change map to top

--- src/components/contact/LinksComp.js
+++ src/components/contact/LinksComp.js
@@ -28,16 +28,15 @@ const Title= styled.h2`
 function LinksComp() {
   return (
       <Container>
+            <a href="https://www.google.com/maps/place/Carrer+d'Enric+Morera,+5,+08902+L'Hospitalet+de+Llobregat,+Barcelona/@41.3626198,2.1253095,17z/data=!3m1!4b1!4m5!3m4!1s0x12a49895739007fb:0x7f9c9de080bbf73a!8m2!3d41.3626198!4d2.1274982" target="_blank" rel="noopener noreferrer">
+                <Title>Maps</Title>
+            </a>
             <a href="https://www.instagram.com/apollo30com/" target="_blank" rel="noopener noreferrer">
                 <Title>Instagram</Title>
             </a>
             <a href="https://www.linkedin.com/company/apollo-30/" target="_blank" rel="noopener noreferrer">
                 <Title>LinkedIn</Title>
             </a>
-            <a href="https://www.google.com/maps/place/Carrer+d'Enric+Morera,+5,+08902+L'Hospitalet+de+Llobregat,+Barcelona/@41.3626198,2.1253095,17z/data=!3m1!4b1!4m5!3m4!1s0x12a49895739007fb:0x7f9c9de080bbf73a!8m2!3d41.3626198!4d2.1274982" target="_blank" rel="noopener noreferrer">
-                <Title>Maps</Title>
-            </a>
-          
       </Container>
   )
 }
