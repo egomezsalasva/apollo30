6d77b8a2626ef45ae12edab85a3442e16836b56f
Kike <kike@Kikes-MacBook-Pro.local>
2020-06-12 20:50:39 +0200

    Progress Bar Structuring

--- src/components/innerProjects/ProgressBar.js
+++ src/components/innerProjects/ProgressBar.js
@@ -5,9 +5,13 @@ import styled from 'styled-components'
 //-Data
 import projectData from '../../data/projectsData'
 
+
+const numberOfSlides = projectData.nike.length
+
 //STYLES
 //-Variables
-
+const w30px = "8vw"
+const heightOfBar = "4px"
 //-Components
 const Container = styled.div`
   position: absolute;
@@ -16,25 +20,49 @@ const Container = styled.div`
   width: 100%;
   background: yellow;
 `
-const BarContainer = styled.div`
-  position: absolute;
+const AllBarsContainer = styled.div`
+  width: calc( 100% - ( ${w30px} * 2 ) );
+  height: ${heightOfBar};
+  position: relative;
   top: 50%;
-  transform: translateY(-50%);
-  height: 4px;
-  width: calc(100% - (w30px * 2) );
+  left: 50%;
+  transform: translate(-50%, -50%);
   background: green;
 `
+const BarContainer = styled.div`
+  position: relative;
+  top: calc( -15px + ${heightOfBar});
+  display: inline-block;
+  width: calc( ( 100% / ${numberOfSlides} ) - 15px);
+  height: 100%;
+  background: pink;
+  margin: 0 7.5px;
+`
 
-const numberOfSlides = projectData.nike.length
+
 
 
 //MAIN COMPONENT
 function ProgressBar() {
   return (
      <Container>
-       <BarContainer>
+       <AllBarsContainer>
+          <BarContainer>
 
-       </BarContainer>
+          </BarContainer>
+          <BarContainer>
+            
+          </BarContainer>
+          <BarContainer>
+            
+          </BarContainer>
+          <BarContainer>
+            
+          </BarContainer>
+          <BarContainer>
+            
+          </BarContainer>
+       </AllBarsContainer>
      </Container>
   )
 }
